549fcaf418bbd7b91f9f7f86bf0ce8272e5267fd
human1264 <95896757+human1264@users.noreply.github.com>
2025-01-24 11:10:55 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -66,14 +66,4 @@ public class PigLatin {
 }else{return("");}
 }
 
-
-	public void setup() {
-	String[] lines = loadStrings("words.txt");
-	System.out.println("there are " + 
-                      lines.length + " lines");
-	for (int i = 0 ; i < lines.length; i++) {
-	  System.out.println(pigLatin(lines[i]));
-	}
-}
-
  // end
